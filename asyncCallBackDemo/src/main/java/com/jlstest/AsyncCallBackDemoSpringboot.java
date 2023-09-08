ce429ed31733ef304c63cad831c82526c30562f0
JLS <15868811005@163.com>
2023-09-08 15:53:20 +0800

    异步回调工程创建

---
+++ asyncCallBackDemo/src/main/java/com/jlstest/AsyncCallBackDemoSpringboot.java
@@ -0,0 +1,17 @@
+package com.jlstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JLS
+ * @description:
+ * @since 2023-09-08 15:51
+ */
+@SpringBootApplication
+public class AsyncCallBackDemoSpringboot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncCallBackDemoSpringboot.class, args);
+    }
+}
