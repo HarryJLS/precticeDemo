b153008b8d7429e4eb7baed66c941d448c2d8ae9
JLS <15868811005@163.com>
2023-10-24 12:24:43 +0800

    单元测试demo

---
+++ demo/src/test/java/com/jlstest/service/impl/AsyncServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.jlstest.service.impl;
+
+import com.jlstest.service.AsyncService;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * @author JLS
+ * @description:
+ * @since 2023-10-18 16:41
+ */
+@SpringBootTest
+public class AsyncServiceImplTest {
+
+    @Resource
+    private AsyncService asyncService;
+
+}
