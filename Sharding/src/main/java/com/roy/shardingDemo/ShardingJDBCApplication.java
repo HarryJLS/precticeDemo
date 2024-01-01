2acd15a64feee8072f1ebf572fe471b2253bf0e9
JLS <15868811005@163.com>
2024-01-01 14:55:40 +0800

    tuling学习资料更新

--- Sharding/src/main/java/com/roy/shardingDemo/ShardingJDBCApplication.java
+++ Sharding/src/main/java/com/roy/shardingDemo/ShardingJDBCApplication.java
@@ -1,6 +1,9 @@
 package com.roy.shardingDemo;
 
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author ：楼兰
