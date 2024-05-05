3817cda606971ed13375448bf572c1c795ec748e
JLS <15868811005@163.com>
2024-05-05 16:13:24 +0800

    demo更新

---
+++ algorithm_prectice/src/main/java/com/prectice/other_prectice/LeakyBucket.java
@@ -0,0 +1,35 @@
+package com.prectice.other_prectice;
+
+/**
+ * @author JLS
+ * @description: 漏桶算法
+ * @since 2024-05-05 15:49
+ */
+public class LeakyBucket {
+
+    // 记录的时间戳
+    public long timeStamp = System.currentTimeMillis();
+    // 漏桶的容量
+    public long capacity;
+    // 漏桶流出的速率
+    public long rate = 1;
+    // 当前的水量
+    public long water;
+
+    public boolean limit() {
+        // 计算出水量
+        long now = System.currentTimeMillis();
+        water = Math.max(0, water - (now - timeStamp) * rate);
+        timeStamp = now;
+        if (water + 1 < capacity) {
+            // 水还未满，继续加水
+            water += 1;
+            return true;
+        } else {
+            // 水满，拒绝加水
+            return false;
+        }
+    }
+
+
+}
---
+++ algorithm_prectice/src/main/java/com/prectice/other_prectice/moveBucket.java
@@ -0,0 +1,52 @@
+package com.prectice.other_prectice;
+
+import java.util.LinkedList;
+
+/**
+ * @author JLS
+ * @description: 滑动时间窗口算法
+ * @since 2024-05-05 15:52
+ */
+public class moveBucket {
+
+    public static LinkedList<Long> data = new LinkedList<>();
+
+    public static long limt = 10;
+
+    public static long capacity = 10;
+
+    public static long counter = 0;
+
+    public static void main(String[] args) {
+        // 线程启动
+        new Thread(() -> {
+            while (true) {
+                try {
+                    doCheck();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
+    // 用于模拟数据的添加
+    public static void addCounter() {
+        counter++;
+    }
+
+    public static boolean doCheck() throws InterruptedException {
+        while (true) {
+            Thread.sleep(1000/10);
+            data.add(counter);
+            if (data.size() > capacity) {
+                data.removeFirst();
+            }
+            if (data.peekLast() - data.peekFirst() < limt) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
